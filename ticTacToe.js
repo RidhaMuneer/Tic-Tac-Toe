0c0638ce07191a1c30ba9014f0e7b517a5439bc7
RidhaMuneer <ridhamuner@gmail.com>
2023-06-22 23:06:22 +0300

    modified the game so that it shows the tie screen when there is a tie, and the boxes will be unclickable once clicked

--- ticTacToe.js
+++ ticTacToe.js
@@ -43,7 +43,25 @@ const game = (() => {
         }else if(marker === 'O'){
             counter = 2;
         }
+    };
+    const showTie = () => {
+        var h1Element = document.createElement('h1');
+        h1Element.textContent = 'IT IS A TIE';
 
+        var blurStyle = 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; backdrop-filter: blur(10px);';
+
+        var containerDiv = document.createElement('div');
+        containerDiv.style.textAlign = 'center';
+        containerDiv.style.cssText = blurStyle + ' display: flex; justify-content: center; align-items: center; flex-direction: column;';
+
+        containerDiv.appendChild(h1Element);
+
+        document.body.appendChild(containerDiv);
+
+        setTimeout(function() {
+            containerDiv.remove();
+        }, 1500);
+        counter = 1;
     };
     const reset = () => {
         box0.textContent = '';
@@ -55,6 +73,10 @@ const game = (() => {
         box6.textContent = '';
         box7.textContent = '';
         box8.textContent = '';
+        for(let i = 0; i < clicked.length; i++){
+            clicked[i] = false;
+        }
+        gameCounter = 0;
     };
     const check = (player1, player2) => {
         if (box0.textContent === 'X' && box1.textContent === 'X' && box2.textContent === 'X') {
@@ -89,106 +111,166 @@ const game = (() => {
             player1.won();
         } else if (box2.textContent === 'O' && box5.textContent === 'O' && box8.textContent === 'O') {
             player2.won();
-        }        
+        } else if (gameCounter == 9){
+            showTie();
+            reset();
+        }
     };
     var counter = 1;
+    var clicked = [false, false, false, false, false, false, false, false, false];
+    var gameCounter = 0;
     const changeContent0 = () => {
-        if(counter % 2 == 1){
-            box0.textContent = 'X';
-            counter++;
-            game.check(player1, player2);
-        }else if(counter % 2 == 0){
-            box0.textContent = 'O';
-            counter++;
-            game.check(player1, player2);
+        if(clicked[0] == false){
+            if(counter % 2 == 1){
+                box0.textContent = 'X';
+                counter++;
+                gameCounter++;
+                clicked[0] = true;
+                game.check(player1, player2);
+            }else if(counter % 2 == 0){
+                box0.textContent = 'O';
+                counter++;
+                gameCounter++;
+                clicked[0] = true;
+                game.check(player1, player2);
+            }
         }
     };
     const changeContent1 = () => {
-        if(counter % 2 == 1){
-            box1.textContent = 'X';
-            counter++;
-            game.check(player1, player2);
-        }else if(counter % 2 == 0){
-            box1.textContent = 'O';
-            counter++;
-            game.check(player1, player2);
+        if(clicked[1] == false){
+            if(counter % 2 == 1){
+                box1.textContent = 'X';
+                counter++;
+                gameCounter++;
+                clicked[1] = true;
+                game.check(player1, player2);
+            }else if(counter % 2 == 0){
+                box1.textContent = 'O';
+                counter++;
+                gameCounter++;
+                clicked[1] = true;
+                game.check(player1, player2);
+            }
         }
     };
     const changeContent2 = () => {
-        if(counter % 2 == 1){
-            box2.textContent = 'X';
-            counter++;
-            game.check(player1, player2);
-        }else if (counter % 2 == 0){
-            box2.textContent = 'O';
-            counter++;
-            game.check(player1, player2);
+        if(clicked[2] == false){
+            if(counter % 2 == 1){
+                box2.textContent = 'X';
+                counter++;
+                gameCounter++;
+                clicked[2] = true;
+                game.check(player1, player2);
+            }else if (counter % 2 == 0){
+                box2.textContent = 'O';
+                counter++;
+                gameCounter++;
+                clicked[2] = true;
+                game.check(player1, player2);
+            }
         }
     };
     const changeContent3 = () => {
-        if(counter % 2 == 1){
-            box3.textContent = 'X';
-            counter++;
-            game.check(player1, player2);
-        }else if (counter % 2 == 0){
-            box3.textContent = 'O';
-            counter++;
-            game.check(player1, player2);
+        if(clicked[3] == false){
+            if(counter % 2 == 1){
+                box3.textContent = 'X';
+                counter++;
+                gameCounter++;
+                clicked[3] = true;
+                game.check(player1, player2);
+            }else if (counter % 2 == 0){
+                box3.textContent = 'O';
+                counter++;
+                gameCounter++;
+                clicked[3] = true;
+                game.check(player1, player2);
+            }
         }
     };
     const changeContent4 = () => {
-        if(counter % 2 == 1){
-            box4.textContent = 'X';
-            counter++;
-            game.check(player1, player2);
-        }else if (counter % 2 == 0){
-            box4.textContent = 'O';
-            counter++;
-            game.check(player1, player2);
+        if(clicked[4] == false){
+            if(counter % 2 == 1){
+                box4.textContent = 'X';
+                counter++;
+                gameCounter++;
+                clicked[4] = true;
+                game.check(player1, player2);
+            }else if (counter % 2 == 0){
+                box4.textContent = 'O';
+                counter++;
+                gameCounter++;
+                clicked[4] = true;
+                game.check(player1, player2);
+            }
         }
     };
     const changeContent5 = () => {
-        if(counter % 2 == 1){
-            box5.textContent = 'X';
-            counter++;
-            game.check(player1, player2);
-        }else if (counter % 2 == 0){
-            box5.textContent = 'O';
-            counter++;
-            game.check(player1, player2);
+        if(clicked[5] == false){
+            if(counter % 2 == 1){
+                box5.textContent = 'X';
+                counter++;
+                gameCounter++;
+                clicked[5] = true;
+                game.check(player1, player2);
+            }else if (counter % 2 == 0){
+                box5.textContent = 'O';
+                counter++;
+                gameCounter++;
+                clicked[5] = true;
+                game.check(player1, player2);
+            }
         }
     };
     const changeContent6 = () => {
-        if(counter % 2 == 1){
-            box6.textContent = 'X';
-            counter++;
-            game.check(player1, player2);
-        }else if (counter % 2 == 0){
-            box6.textContent = 'O';
-            counter++;
-            game.check(player1, player2);
+        if(clicked[6] == false){
+            if(counter % 2 == 1){
+                box6.textContent = 'X';
+                counter++;
+                gameCounter++;
+                clicked[6] = true;
+                game.check(player1, player2);
+            }else if (counter % 2 == 0){
+                box6.textContent = 'O';
+                counter++;
+                gameCounter++;
+                clicked[6] = true;
+                game.check(player1, player2);
+            }
         }
     };
     const changeContent7 = () => {
-        if(counter % 2 == 1){
-            box7.textContent = 'X';
-            counter++;
-            game.check(player1, player2);
-        }else if (counter % 2 == 0){
-            box7.textContent = 'O';
-            counter++;
-            game.check(player1, player2);
+        if(clicked[7] == false){
+            if(counter % 2 == 1){
+                box7.textContent = 'X';
+                counter++;
+                gameCounter++;
+                clicked[7] = true;
+                game.check(player1, player2);
+            }else if (counter % 2 == 0){
+                box7.textContent = 'O';
+                counter++;
+                gameCounter++;
+                clicked[7] = true;
+                game.check(player1, player2);
+            }
         }
     };
     const changeContent8 = () => {
-        if(counter % 2 == 1){
-            box8.textContent = 'X';
-            counter++;
-            game.check(player1, player2);
-        }else if (counter % 2 == 0){
-            box8.textContent = 'O';
-            counter++;
-            game.check(player1, player2);
+        if(clicked[8] == false){
+            if(counter % 2 == 1){
+                box8.textContent = 'X';
+                counter++;
+                gameCounter++;
+                clicked[8] = true;
+                game.check(player1, player2);
+            }else if (counter % 2 == 0){
+                box8.textContent = 'O';
+                counter++;
+                gameCounter++;
+                clicked[8] = true;
+                game.check(player1, player2);
+            }
+
         }
     };
     return {
@@ -203,6 +285,7 @@ const game = (() => {
       changeContent7,
       changeContent8,
       reset,
-      showScore
+      showScore,
+      showTie
     };
 })();
